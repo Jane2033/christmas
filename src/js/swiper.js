2009446d094d2a30e2a1977f3a2acab04a8d7ce8
Anya <zhangyazhen@zhangyahendeMBP.home>
2024-11-27 20:01:06 +0800

    first commit

---
+++ src/js/swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    rewind: true,
+    autoplay: true,
+    grabCursor: 'true',
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
